abf24b0a076bfa7e8757c7c15977422104489a01
Oleksandr Shchur <71557324+OleksandrShchur@users.noreply.github.com>
2026-05-22 12:34:17 +0300

    add health check endpoint (#4)

---
+++ AccuScraperWebWithReact.Server/Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccuScraperWebWithReact.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        [HttpHead]
+        public IActionResult Get() => Ok("Accu Web Scraper is healthy!");
+    }
+}
